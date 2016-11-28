5cca86022434fbf384d73303c5be04330e90bf96
judasnow <judasnow@gmail.com>
2016-11-28 19:16:53 +0800

    tree

---
+++ tree/tree.go
@@ -0,0 +1,24 @@
+package main
+
+// https://github.com/nicmart/Tree
+
+import (
+)
+
+type Node struct {
+	Parent *Node
+	Name   string
+	Weight int
+	Next   *Node
+}
+
+func (Node) New() {
+
+}
+
+func (Node) SetValue() {
+
+}
+
+func main() {
+}
